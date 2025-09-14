ec5432a51be23674965d95e21fef4c0c2c3e9195
dodohan1224 <kdymmm1224@naver.com>
2025-09-14 20:28:57 +0900

    [refactor] springsecurity 전체 접근 허용

--- src/main/java/com/solarsido/solarlog_be/config/SecurityConfig.java
+++ src/main/java/com/solarsido/solarlog_be/config/SecurityConfig.java
@@ -36,8 +36,7 @@ public class SecurityConfig {
         .httpBasic(AbstractHttpConfigurer::disable)
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 사용 안 함
         .authorizeHttpRequests(authorize -> authorize
-            .requestMatchers("/api/v1/signup", "/api/v1/signup/check-id", "/api/v1/users/login","/api/v1/history").permitAll()
-            .anyRequest().authenticated()
+            .anyRequest().permitAll()
         )
         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
