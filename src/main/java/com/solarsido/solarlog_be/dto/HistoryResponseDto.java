3d024dfd05264b95413a60f0dc4cea9803c3bf60
dodohan1224 <kdymmm1224@naver.com>
2025-09-16 21:57:30 +0900

    [refactor] 히스토리 리스트 응답 방식 변경

--- src/main/java/com/solarsido/solarlog_be/dto/HistoryResponseDto.java
+++ src/main/java/com/solarsido/solarlog_be/dto/HistoryResponseDto.java
@@ -3,7 +3,6 @@ package com.solarsido.solarlog_be.dto;
 import com.solarsido.solarlog_be.entity.EventDetail;
 import com.solarsido.solarlog_be.entity.EventType;
 import java.time.LocalDateTime;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,21 +13,11 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class HistoryResponseDto {
-
-  private boolean success;
-  private List<AlarmList> alarmLists;
-
-  @Getter
-  @Setter
-  @AllArgsConstructor
-  @NoArgsConstructor
-  public static class AlarmList {
     private Long alarmId;
     private LocalDateTime alarmDate;
     private String modelName;
     private EventType eventType;
     private EventDetail eventDetail;
     private boolean isRead;
-  }
 
 }
