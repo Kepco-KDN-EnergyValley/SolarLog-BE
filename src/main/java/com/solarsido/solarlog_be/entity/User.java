9af3b71983b6657f1aaea5fb12c61ca484bd1f17
dodohan1224 <kdymmm1224@naver.com>
2025-09-23 17:05:34 +0900

    [refactor] fcm 알림 생성

--- src/main/java/com/solarsido/solarlog_be/entity/User.java
+++ src/main/java/com/solarsido/solarlog_be/entity/User.java
@@ -1,10 +1,14 @@
 package com.solarsido.solarlog_be.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -16,6 +20,10 @@ public class User {
 
   private String userId;
   private String password;
+
+  // 유저가 여러 개의 FCM 토큰을 가질 수 있음
+  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<Fcm> fcmTokens = new ArrayList<>();
 
   public User() {
   }
@@ -49,5 +57,12 @@ public class User {
 
   public void setPassword(String password) {
     this.password = password;
+  }
+  public List<Fcm> getFcmTokens() {   // ✅ 이게 필요함
+    return fcmTokens;
+  }
+
+  public void setFcmTokens(List<Fcm> fcmTokens) {
+    this.fcmTokens = fcmTokens;
   }
 }
